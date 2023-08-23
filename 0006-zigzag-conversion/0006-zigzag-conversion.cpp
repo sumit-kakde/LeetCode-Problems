001c2ba9c3eded254dd92e993338b4f4387f420d
sumit-kakde <115872015+sumit-kakde@users.noreply.github.com>
2023-08-23 19:47:53 +0530

    Time: 8 ms (76.12%), Space: 10.3 MB (68.58%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) {
+        return s; // No need to convert
+    }
+
+    vector<string> rows(min(numRows, int(s.length()))); // Create rows
+
+    int curRow = 0;
+    bool goingDown = false;
+
+    for (char c : s) {
+        rows[curRow] += c;
+
+        if (curRow == 0 || curRow == numRows - 1) {
+            goingDown = !goingDown;
+        }
+
+        curRow += goingDown ? 1 : -1;
+    }
+
+    string result;
+    for (string row : rows) {
+        result += row;
+    }
+
+    return result;  
+    }
+};
